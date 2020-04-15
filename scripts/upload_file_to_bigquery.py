faff727e68bea15c249d2235ef8884e4575986b6
loic.macherel <loic.macherel@gmail.com>
2020-04-15 21:49:02 +0200

    Import read geodataframe

--- scripts/upload_file_to_bigquery.py
+++ scripts/upload_file_to_bigquery.py
@@ -1,15 +1,11 @@
 """Upload the data contained in a csv file into bigquery."""
 
 import argparse
-import sys
 
-import geopandas
 from google.cloud import bigquery
 import pandas
-import shapely.wkt
 
-
-expected_columns = {"id", "geometry", "epsg"}
+from utils import read_geodataframe_from_csv
 
 
 def parse_args():
@@ -31,16 +27,6 @@ def get_table_schema(table_name):
          } for field in schema
     ]
     return json_schema
-
-
-def read_geodataframe_from_csv(file_path: str) -> geopandas.GeoDataFrame:
-    """Read a DataFrame with pandas, then convert its geometry field to a shapely object.
-    Ensures that geometric fields are valid before uploading them."""
-    df = pandas.read_csv(file_path)
-    assert set(df.columns) == expected_columns, \
-        f"columns in the file are {df.columns}, they must be {expected_columns}"
-    df = geopandas.GeoDataFrame(df, geometry=df['geometry'].apply(shapely.wkt.loads))
-    return df
 
 
 def main(file_paths: str, data_type: str):
